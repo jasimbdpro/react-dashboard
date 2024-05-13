9cd5d1b2762f186303c1327511b35bca849d510b
Jasim Uddin <jasimbdpro@gmail.com>
2024-05-13 21:38:44 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NewChart from './components/NewChart/NewChart';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Chart Practice Using Recharts<br /><span className='gray2'>(a chart library for React)</span></h2>
+      <NewChart></NewChart>
+    </div>
+  );
+}
+
+export default App;
